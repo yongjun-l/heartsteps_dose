3a148161ee95093c734a5011afab46efce1debe5
Yongjun Lee <yongjun.lee5@gmail.com>
2024-12-12 02:52:52 -0700

    updated Tom's presentation

--- R/estimating_equations.R
+++ R/estimating_equations.R
@@ -6,7 +6,7 @@
 #' @param total_doses number of doses given in a day
 #'
 #' @return a matrix of all possible dose combinations
-#' @examples 
+#' @examples
 #' # This is an internal function
 generate_regimes <- function(time_points, total_doses) {
   if (total_doses == 0) {
@@ -30,7 +30,7 @@ generate_regimes <- function(time_points, total_doses) {
 #' @param p probability of single treatment
 #'
 #' @return a list of cumulative dose, probability of a_t, and dose weight
-#' @examples 
+#' @examples
 #' # This is an internal function
 get_p_a <- function(a, p) {
   cat("a")
@@ -65,48 +65,48 @@ get_p_a <- function(a, p) {
 #' @return a vector of estimating equation
 #' @examples
 #' # This is an internal function
-ee <- function( beta, df.wide, p_a, time, n.days, dose, y, trt, 
+ee <- function( beta, df.wide, p_a, time, n.days, dose, y, trt,
                 baseline=NULL, timevar=NULL, b.prime=NULL, t.prime=NULL) {
-  
+
   p <- 0.5; U <- 0; zeros <- lapply(1:5, rep, x = 0)
   a_5 <- generate_regimes(time, dose)
-  
+
   if (!is.null(b.prime)) {
     ate <- cbind(1, df.wide[,b.prime]) |> as.matrix()
   } else {
     ate <- rep(1, n.days) |> as.matrix()
   }
-  
+
   for (decision in 1:time) {
     y.name <- paste0(y,"_", decision)
     a.name <- paste0(trt,"_", decision)
     if (!is.null(timevar)) {s.name <- paste0(timevar,"_", decision)} else {s.name <- NULL}
     formula <- as.formula(paste(y.name, paste(c(baseline, s.name, a.name), collapse = " + "), sep = " ~ "))
     fit <- lm(formula, data = df.wide)
-    
+
     if (!is.null(t.prime)) {
       ate.name <- c(b.prime,paste0(t.prime,"_", decision))
       ate <- cbind(1, df.wide[,ate.name]) |> as.matrix()
     }
-    
+
     df.m0 <- df.wide[,c(y.name, baseline, s.name, a.name)]
     df.m1 <- df.wide[,c(y.name, baseline, s.name, a.name)]
-    
+
     df.m0[,a.name] <- FALSE
-    
+
     a.interest <- df.wide[, paste(trt, "_", 1:decision, sep="")] |> as.matrix()
     y.decision <- df.wide[,y.name] |> as.matrix()
     p.decision <- p_a[,decision]
     zeros.decision <- zeros[[decision]]
     for (regime in 1:nrow(a_5)) {
-      
+
       df.m1[,a.name] <- as.logical(a_5[regime, decision])
       m1 <- predict(fit, newdata = df.m1)
       m0 <- predict(fit, newdata = df.m0)
-      
+
       I_at <- rowSums(a.interest == matrix(a_5[regime, 1:decision], nrow=n.days, ncol=decision, byrow=TRUE)) == decision
       I_0t <- rowSums(a.interest == matrix(zeros.decision, nrow=n.days, ncol=decision, byrow=TRUE)) == decision
-      
+
       U <- U + t(ate) %*% (I_at/p.decision * (y.decision - m1) -
                              I_0t/p.decision * (y.decision - m0) +
                              m1 - m0 - ( as.matrix(ate) %*% beta )/time)
@@ -127,8 +127,7 @@ ee <- function( beta, df.wide, p_a, time, n.days, dose, y, trt,
 #' @param ... model specification
 #'
 #' @return a vector of coefficients
-#' @importFrom dplyr mutate across everything 
-#' @importFrom purrr map_dbl
+#' @importFrom dplyr mutate across everything
 #' @examples
 #' # This is an internal function
 CeeDose <- function(df, id, day, slot, p, dose, ...) {
@@ -148,11 +147,11 @@ CeeDose <- function(df, id, day, slot, p, dose, ...) {
   n.days <- nrow(df.wide);n.beta <- length(b.prime)+length(t.prime)+1
   a <- df.wide[, paste(trt, "_", 1:time, sep="")]
   a.mat <- as.matrix(a)
-  a.mat2 <- a %>%
-    mutate(across(everything(), ~ map_dbl(.x, 1))) %>%  # Extract the first element from each inner list
-    as.matrix()
-  
-  matrix <- get_p_a(a.mat2, p)
+  #a.mat2 <- a %>%
+  #  mutate(across(everything(), ~ map_dbl(.x, 1))) %>%  # Extract the first element from each inner list
+  #  as.matrix()
+
+  matrix <- get_p_a(a.mat, p)
   cum_d <- matrix$cum_d
   p_a <- matrix$p_a
   init_beta <- rep(0,n.beta)
@@ -184,7 +183,7 @@ CeeDose <- function(df, id, day, slot, p, dose, ...) {
 #' @import data.table
 #' @export
 #' @examples
-#' m=5; n=10; time=5; days=2; eta=c(3,2,1,4); rho=0.5; theta1=c(3,2,1); theta2=0; 
+#' m=5; n=10; time=5; days=2; eta=c(3,2,1,4); rho=0.5; theta1=c(3,2,1); theta2=0;
 #' beta10=1; beta11=0.5; beta12=0.3; p=0.5; dose=1
 #' dfs <- simMhealth(m, n, time, days, eta, rho, theta1, theta2, beta10, beta11, beta12, p)
 #' mHealthDose(dfs, id="ID", day="DAY", slot="SLOT", p,
@@ -195,21 +194,21 @@ CeeDose <- function(df, id, day, slot, p, dose, ...) {
 mHealthDose <- function(df, id, day, slot, p, dose, ...,
                         boot = 100, cores = 1, cl = NULL, sim = FALSE,
                         print_progress=FALSE) {
-  
+
   if (sim) { boot_dfs <- df } else { boot_dfs <- boot_samples(df, id, B=boot) }
-  
+
   # Calling parallel
   if ((cores > 1) | length(cl)) {
-    
+
     # Creating the cluster
     if (!length(cl)) {
       cl <- parallel::makeCluster(cores)
       on.exit(parallel::stopCluster(cl))
-      
+
       # Loading R packages
       parallel::clusterEvalQ(cl, library(mrt.dose))
     }
-    
+
     # Calling the function
     rslts <- parallel::parSapply(cl, X=seq_len(boot), function(i, CeeDose, df, id, day, slot, p, dose, sim, boot_dfs, ...) {
       if (sim) {
@@ -222,10 +221,10 @@ mHealthDose <- function(df, id, day, slot, p, dose, ...,
       # Call the CeeDose function with all required arguments
       return(CeeDose(boot_df, boot_id, day, slot, p, dose, ...))
     }, CeeDose = mrt.dose:::CeeDose, df = df, id = id, day = day, slot = slot, p = p, dose = dose, sim = sim, boot_dfs = boot_dfs, ...)
-    
-    
+
+
   } else {
-    
+
     # If no parallel apply
     rslts <- sapply(X = seq_len(boot), function(i, CeeDose, df, id, day, slot, p, dose, sim, boot_dfs, ...) {
       if (sim) {
@@ -238,22 +237,22 @@ mHealthDose <- function(df, id, day, slot, p, dose, ...,
       # Call the CeeDose function with all required arguments
       return(CeeDose(boot_df, boot_id, day, slot, p, dose, ...))
     }, CeeDose = mrt.dose:::CeeDose, df = df, id = id, day = day, slot = slot, p = p, dose = dose, sim = sim, boot_dfs = boot_dfs, ...)
-    
+
   }
   args <- list(...)
   trt <- args$trt; b.prime <- args$b.prime; t.prime <- args$t.prime
-  rownames(rslts) <- c(paste0(trt, dose, sep=""), paste(paste0(trt, dose, sep=""), c(b.prime, t.prime), sep=":")) 
-  
+  rownames(rslts) <- c(paste0(trt, dose, sep=""), paste(paste0(trt, dose, sep=""), c(b.prime, t.prime), sep=":"))
+
   std.error <- apply(rslts, 1, sd)
-  if (sim) 
+  if (sim)
     point <- rowMeans(rslts)
-  else 
+  else
     point <- CeeDose(df, id, day, slot, p, dose, ...)
   names(point) <- c(paste0(trt, dose, sep=""), paste(paste0(trt, dose, sep=""), c(b.prime, t.prime), sep=":"))
-  
+
   return(structure(list(
     coefficients = point,
-    std.error = std.error, 
+    std.error = std.error,
     ...), class = "mHealthDose")
   )
 }
@@ -271,7 +270,7 @@ mHealthDose <- function(df, id, day, slot, p, dose, ...,
 #' @return list of mHealthDose objects
 #' @export
 #' @examples
-#' m=5; n=10; time=5; days=5; eta=c(0,0,0,0); rho=0; theta1=c(0,0,0); theta2=0; 
+#' m=5; n=10; time=5; days=5; eta=c(0,0,0,0); rho=0; theta1=c(0,0,0); theta2=0;
 #' beta10=5; beta11=c(0,0); beta12=c(0,0); p=0.5; dose=1
 #' dfs <- simMhealth(m, n, time, days, eta, rho, theta1, theta2, beta10, beta11, beta12, p)
 #' fits <- mHealthDoses(dfs, id="ID", day="DAY", slot="SLOT", p,
@@ -297,7 +296,7 @@ mHealthDoses <- function(df, id, day, slot, p, doses, ...) {
 #' @return invisible
 #' @export
 #' @examples
-#' m=5; n=10; time=5; days=5; eta=c(0,0,0,0); rho=0; theta1=c(0,0,0); theta2=0; 
+#' m=5; n=10; time=5; days=5; eta=c(0,0,0,0); rho=0; theta1=c(0,0,0); theta2=0;
 #' beta10=5; beta11=c(0,0); beta12=c(0,0); p=0.5; dose=1
 #' dfs <- simMhealth(m, n, time, days, eta, rho, theta1, theta2, beta10, beta11, beta12, p)
 #' fit <- mHealthDose(dfs[[1]], id="ID", day="DAY", slot="SLOT", p,
@@ -323,7 +322,7 @@ print.mHealthDose <- function(x, ...) {
 #' @return coefficients
 #' @export
 #' @examples
-#' m=5; n=10; time=5; days=5; eta=c(0,0,0,0); rho=0; theta1=c(0,0,0); theta2=0; 
+#' m=5; n=10; time=5; days=5; eta=c(0,0,0,0); rho=0; theta1=c(0,0,0); theta2=0;
 #' beta10=5; beta11=c(0,0); beta12=c(0,0); p=0.5; dose=1
 #' dfs <- simMhealth(m, n, time, days, eta, rho, theta1, theta2, beta10, beta11, beta12, p)
 #' fit <- mHealthDose(dfs[[1]], id="ID", day="DAY", slot="SLOT", p,
@@ -349,7 +348,7 @@ coef.mHealthDose <- function(object, param=NULL) {
 #' @return custom print function for class mHealthDose
 #' @export
 #' @examples
-#' m=5; n=10; time=5; days=5; eta=c(0,0,0,0); rho=0; theta1=c(0,0,0); theta2=0; 
+#' m=5; n=10; time=5; days=5; eta=c(0,0,0,0); rho=0; theta1=c(0,0,0); theta2=0;
 #' beta10=5; beta11=c(0,0); beta12=c(0,0); p=0.5; dose=1
 #' dfs <- simMhealth(m, n, time, days, eta, rho, theta1, theta2, beta10, beta11, beta12, p)
 #' fit <- mHealthDose(dfs[[1]], id="ID", day="DAY", slot="SLOT", p,
@@ -362,16 +361,16 @@ confint.mHealthDose <- function(object, param, level = 0.95) {
   cf <- coef.mHealthDose(object)
   ses <- object$std.error
   pnames <- names(ses)
-  if (is.matrix(cf)) 
+  if (is.matrix(cf))
     cf <- setNames(as.vector(cf), pnames)
-  if (missing(param)) 
+  if (missing(param))
     param <- pnames
-  else if (is.numeric(param)) 
+  else if (is.numeric(param))
     param <- pnames[param]
   a <- (1 - level)/2
   a <- c(a, 1 - a)
   pct <- paste0(round(a * 100, 3), "%")
-  ci <- array(NA_real_, dim = c(length(param), 2L), dimnames = list(param, 
+  ci <- array(NA_real_, dim = c(length(param), 2L), dimnames = list(param,
                                                                     pct))
   ci[] <- cf[param] + ses[param] %o% qnorm(a)
   return(ci)
@@ -386,7 +385,7 @@ confint.mHealthDose <- function(object, param, level = 0.95) {
 #' @importFrom stats pnorm setNames qnorm
 #' @export
 #' @examples
-#' m=5; n=10; time=5; days=5; eta=c(0,0,0,0); rho=0; theta1=c(0,0,0); theta2=0; 
+#' m=5; n=10; time=5; days=5; eta=c(0,0,0,0); rho=0; theta1=c(0,0,0); theta2=0;
 #' beta10=5; beta11=c(0,0); beta12=c(0,0); p=0.5; dose=1
 #' dfs <- simMhealth(m, n, time, days, eta, rho, theta1, theta2, beta10, beta11, beta12, p)
 #' fit <- mHealthDose(dfs[[1]], id="ID", day="DAY", slot="SLOT", p,
@@ -399,12 +398,12 @@ pval <- function(object, param) {
   cf <- coef.mHealthDose(object)
   ses <- object$std.error
   pnames <- names(ses)
-  
-  if (is.matrix(cf)) 
+
+  if (is.matrix(cf))
     cf <- setNames(as.vector(cf), pnames)
-  if (missing(param)) 
+  if (missing(param))
     param <- pnames
-  else if (is.numeric(param)) 
+  else if (is.numeric(param))
     param <- pnames[param]
   pval <- 2 * (1 - pnorm(abs(cf[param]/ses[param])))
   pval <- setNames(pval, param)
@@ -418,9 +417,9 @@ pval <- function(object, param) {
 #'
 #' @return the summary object
 #' @importFrom stats as.formula
-#' @export 
+#' @export
 #' @examples
-#' m=5; n=10; time=5; days=5; eta=c(0,0,0,0); rho=0; theta1=c(0,0,0); theta2=0; 
+#' m=5; n=10; time=5; days=5; eta=c(0,0,0,0); rho=0; theta1=c(0,0,0); theta2=0;
 #' beta10=5; beta11=c(0,0); beta12=c(0,0); p=0.5; dose=1
 #' dfs <- simMhealth(m, n, time, days, eta, rho, theta1, theta2, beta10, beta11, beta12, p)
 #' fit <- mHealthDose(dfs[[1]], id="ID", day="DAY", slot="SLOT", p,
@@ -438,10 +437,10 @@ summary.mHealthDose <- function(object, ...) {
       paste(c(object$trt, paste(object$trt, c(object$b.prime, object$t.prime), sep=":")), collapse = " + ")
     )
   )
-  coefficients = cbind("Estimate"=coef.mHealthDose(object), 
-                       "Std. Error"=object$std.error, 
-                       "z value"=abs(coef.mHealthDose(object)/object$std.error), 
-                       confint.mHealthDose(object), 
+  coefficients = cbind("Estimate"=coef.mHealthDose(object),
+                       "Std. Error"=object$std.error,
+                       "z value"=abs(coef.mHealthDose(object)/object$std.error),
+                       confint.mHealthDose(object),
                        "p-val"=pval(object))
   return(
     structure(
@@ -458,10 +457,10 @@ summary.mHealthDose <- function(object, ...) {
 #' @param x summary.mHealthDose object
 #' @param ... optional
 #'
-#' @return invisible 
+#' @return invisible
 #' @export
 #' @examples
-#' m=5; n=10; time=5; days=5; eta=c(0,0,0,0); rho=0; theta1=c(0,0,0); theta2=0; 
+#' m=5; n=10; time=5; days=5; eta=c(0,0,0,0); rho=0; theta1=c(0,0,0); theta2=0;
 #' beta10=5; beta11=c(0,0); beta12=c(0,0); p=0.5; dose=1
 #' dfs <- simMhealth(m, n, time, days, eta, rho, theta1, theta2, beta10, beta11, beta12, p)
 #' fit <- mHealthDose(dfs[[1]], id="ID", day="DAY", slot="SLOT", p,
@@ -487,7 +486,7 @@ print.summary.mHealthDose <- function(x, ...) {
 #' @return plot dose response relationship
 #' @export
 #' @examples
-#' m=5; n=10; time=5; days=10; eta=c(3,2,1,4); rho=0.5; theta1=c(3,2,1); theta2=0; 
+#' m=5; n=10; time=5; days=10; eta=c(3,2,1,4); rho=0.5; theta1=c(3,2,1); theta2=0;
 #' beta10=5; beta11=c(3,2); beta12=c(2,1); p=0.5; dose=1
 #' dfs <- simMhealth(m, n, time, days, eta, rho, theta1, theta2, beta10, beta11, beta12, p)
 #' fits <- mHealthDoses(dfs, id="ID", day="DAY", slot="SLOT", p,
@@ -516,8 +515,8 @@ summary.mHealthDoses <- function(object, ...) {
 #' @import ggplot2
 #' @export
 #'
-#' @examples 
-#' m=5; n=10; time=5; days=10; eta=c(3,2,1,4); rho=0.5; theta1=c(3,2,1); theta2=0; 
+#' @examples
+#' m=5; n=10; time=5; days=10; eta=c(3,2,1,4); rho=0.5; theta1=c(3,2,1); theta2=0;
 #' beta10=5; beta11=c(3,2); beta12=c(2,1); p=0.5; dose=1
 #' dfs <- simMhealth(m, n, time, days, eta, rho, theta1, theta2, beta10, beta11, beta12, p)
 #' fits <- mHealthDoses(dfs, id="ID", day="DAY", slot="SLOT", p,
@@ -526,9 +525,9 @@ summary.mHealthDoses <- function(object, ...) {
 #'                      b.prime=c("H1", "H2"), t.prime=c("S1", "S2"),
 #'                      boot = m, cores = 1, sim = TRUE)
 #' sum <- summary(fits)
-#' varlevels <- rbind(H1 = c("Male", "Female"), 
-#'                    S1 = c("Student", "Non-student"), 
-#'                    H2 = c("Mean Age", "Mean Age+1"), 
+#' varlevels <- rbind(H1 = c("Male", "Female"),
+#'                    S1 = c("Student", "Non-student"),
+#'                    H2 = c("Mean Age", "Mean Age+1"),
 #'                    S2 = c("Physical Low", "Physical High"))
 #' colnames(varlevels) <- c("Main Effect", "Interaction Effect")
 #' p <- plot(sum, varlevels)
@@ -542,29 +541,29 @@ plot.mHealthDoses.summary <- function(x, y, ...) {
       Interaction = ifelse(grepl(":", terms), sub(".*:", "", terms), "None"), # Extract interaction or "None"
       EffectType = ifelse(Interaction == "None", "Main Effect", "Interaction Effect"),
       Estimate = Estimate,
-      se = `Std. Error`, 
+      se = `Std. Error`,
       lower = `2.5%`,
       upper = `97.5%`
-    ) 
-  
+    )
+
   # Duplicate main effects for each interaction type
   x.m <- x.df2 |>
     filter(Interaction == "None") |>
     slice(rep(1:n(), each = 4))  # Repeat rows for each interaction
   x.m$Interaction<-rep(c("H1", "H2", "S1", "S2"), times = nrow(x.m) / 4)
-  
+
   # Combine main effects with interactions
   x.int <- x.df2 |>
     filter(Interaction != "None")
   x.int[,4:7] <- x.m[,4:7] + x.int[,4:7]
-  
+
   x.f <- bind_rows(x.m, x.int)
   x.f$labels <- diag(y[x.f$Interaction, x.f$EffectType])
-  
+
   plots <- list()
   for( var in 1:nrow(y)) {
     plot_data_var <- x.f |> filter(Interaction == rownames(y)[var])
-    plots[[rownames(y)[var]]] <- ggplot(plot_data_var, aes(x=Treatment, y=Estimate, fill=labels)) + 
+    plots[[rownames(y)[var]]] <- ggplot(plot_data_var, aes(x=Treatment, y=Estimate, fill=labels)) +
       geom_bar(
         aes(y=Estimate, group=labels),
         stat = "identity",
@@ -590,8 +589,8 @@ plot.mHealthDoses.summary <- function(x, y, ...) {
         legend.title = element_text(size = 10),       # Adjust title size
         legend.text = element_text(size = 8),         # Adjust text size
         legend.key.size = unit(0.5, "cm"),            # Adjust key size
-        legend.spacing.y = unit(0.1, "cm")  
-      ) 
+        legend.spacing.y = unit(0.1, "cm")
+      )
   }
   plots[["all"]] <- grid.arrange(grobs = plots, ncol = 2)
   plots[["all"]]
